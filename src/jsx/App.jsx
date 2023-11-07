f39f8f4fb7838251ec2f037ca20ed971eb589b1f
Teemo Tebest <teemo.tebest@gmail.com>
2023-11-07 22:31:18 +0100

    Updates

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -25,17 +25,18 @@ function App() {
       // Initialize the chart
       Highcharts.mapChart('map_container', {
         accessibility: {
-          description: 'Map where city locations have been defined using latitude/longitude.'
+          description: ''
         },
         chart: {
-          map: topology,
-          height: 500
+          height: Math.max(window.innerHeight, 800),
+          map: topology
         },
         credits: {
           enabled: false
         },
         legend: {
-          enabled: false
+          enabled: true,
+          verticalAlign: 'top'
         },
         mapNavigation: {
           enabled: true
@@ -52,200 +53,186 @@ function App() {
           }
         },
         series: [{
-
           borderColor: '#fff',
           borderWidth: 2,
+          color: '#2ab7ca',
+          data: [{
+            name: 'Schwyz',
+          }, {
+            name: 'Genève'
+          }, {
+            name: 'Valais'
+          }, {
+            name: 'Solothurn'
+          }, {
+            name: 'Vaud',
+          }, {
+            name: 'Ticino',
+          }, {
+            name: 'Jura',
+          }, {
+            name: 'Zürich'
+          }, {
+            name: 'Neuchâtel'
+          }, {
+            name: 'Graubünden'
+          }, {
+            name: 'Luzern'
+          }, {
+            name: 'Fribourg'
+          }, {
+            name: 'St. Gallen'
+          }, {
+            name: 'Schaffhausen'
+          }, {
+            name: 'Bern'
+          }, {
+            name: 'Glarus'
+          }],
+          joinBy: ['kan_name', 'name'],
+          name: 'Visited Kanton',
+          nullColor: '#f4f4f8',
+          showInLegend: true,
           tooltip: {
             headerFormat: '',
             pointFormat: '<b>{point.kan_name}</b>'
-          },
-          data: [{
-            name: 'Schwyz',
-            color: '#2ab7ca'
-          }, {
-            name: 'Genève',
-            color: '#2ab7ca'
-          }, {
-            name: 'Valais',
-            color: '#2ab7ca'
-          }, {
-            name: 'Solothurn',
-            color: '#2ab7ca'
-          }, {
-            name: 'Vaud',
-            color: '#2ab7ca'
-          }, {
-            name: 'Ticino',
-            color: '#2ab7ca'
-          }, {
-            name: 'Jura',
-            color: '#2ab7ca'
-          }, {
-            name: 'Zürich',
-            color: '#2ab7ca'
-          }, {
-            name: 'Neuchâtel',
-            color: '#2ab7ca'
-          }, {
-            name: 'Graubünden',
-            color: '#2ab7ca'
-          }, {
-            name: 'Luzern',
-            color: '#2ab7ca'
-          }, {
-            name: 'Fribourg',
-            color: '#2ab7ca'
-          }, {
-            name: 'St. Gallen',
-            color: '#2ab7ca'
-          }, {
-            name: 'Schaffhausen',
-            color: '#2ab7ca'
-          }, {
-            name: 'Bern',
-            color: '#2ab7ca'
-          }, {
-            name: 'Glarus',
-            color: '#2ab7ca'
-          }],
-          name: 'Switzerland',
-          nullColor: '#f4f4f8',
-          showInLegend: false,
-          joinBy: ['kan_name', 'name'],
+          }
         }, {
+          color: '#000',
+          data: [{
+            lat: 46.2044,
+            lon: 6.1432,
+            name: 'Genève'
+          }, {
+            lat: 46.1091,
+            lon: 7.9297,
+            name: 'Saas-Fee'
+          }, {
+            lat: 46.1670,
+            lon: 8.7943,
+            name: 'Locarno'
+          }, {
+            lat: 46.5594,
+            lon: 7.8927,
+            name: 'Mürren'
+          }, {
+            lat: 47.3769,
+            lon: 8.5417,
+            name: 'Zürich'
+          }, {
+            lat: 47.4789,
+            lon: 9.4917,
+            name: 'Rorschach'
+          }, {
+            lat: 46.7969,
+            lon: 10.2977,
+            name: 'Scuol'
+          }, {
+            lat: 47.0502,
+            lon: 8.3093,
+            name: 'Lucerne'
+          }, {
+            lat: 46.1978,
+            lon: 7.1753,
+            name: 'Ozronnas'
+          }, {
+            lat: 46.2984,
+            lon: 7.0550,
+            name: 'Villars-sur-Ollon'
+          }, {
+            lat: 46.0801,
+            lon: 7.2105,
+            name: 'Le Châble'
+          }, {
+            lat: 46.0282,
+            lon: 7.1471,
+            name: 'Orsières'
+          }, {
+            lat: 47.3500,
+            lon: 7.9037,
+            name: 'Olten'
+          }, {
+            lat: 47.1492,
+            lon: 9.1401,
+            name: 'Amden'
+          }, {
+            lat: 46.6863,
+            lon: 7.8632,
+            name: 'Interlaken'
+          }],
+          marker: {
+            symbol: 'circle',
+          },
+          name: 'Spend a night',
           tooltip: {
             headerFormat: '',
             pointFormat: '<b>{point.name}</b>'
           },
-          type: 'mappoint',
-          name: 'Cities',
-          color: '#000',
+          type: 'mappoint'
+        }, {
+          color: '#ccc',
           data: [{
-            name: 'Genève',
-            lat: 46.2044,
-            lon: 6.1432
-          }, {
-            name: 'Saas-Fee',
-            lat: 46.1091,
-            lon: 7.9297
-          }, {
-            name: 'Locarno',
-            lat: 46.1670,
-            lon: 8.7943
-          }, {
-            name: 'Mürren',
-            lat: 46.5594,
-            lon: 7.8927
-          }, {
-            name: 'Zürich',
-            lat: 47.3769,
-            lon: 8.5417
-          }, {
-            name: 'Rorschach',
-            lat: 47.4789,
-            lon: 9.4917
-          }, {
-            name: 'Scuol',
-            lat: 46.7969,
-            lon: 10.2977
-          }, {
-            name: 'Lucerne',
-            lat: 47.0502,
-            lon: 8.3093
-          }, {
-            name: 'Ozronnas',
-            lat: 46.1978,
-            lon: 7.1753
-          }, {
-            name: 'Villars-sur-Ollon',
-            lat: 46.2984,
-            lon: 7.0550
-          }, {
-            name: 'Le Châble',
-            lat: 46.0801,
-            lon: 7.2105
-          }, {
-            name: 'Orsières',
-            lat: 46.0282,
-            lon: 7.1471
-          }, {
-            name: 'Olten',
-            lat: 47.3500,
-            lon: 7.9037
-          }, {
-            name: 'Amden',
-            lat: 47.1492,
-            lon: 9.1401
-          }, {
-            name: 'Interlaken',
-            lat: 46.6863,
-            lon: 7.8632
-          }, {
-            name: 'Solothurn',
-            color: '#fff',
             lat: 47.2088,
-            lon: 7.5323
+            lon: 7.5323,
+            name: 'Solothurn'
           }, {
-            name: 'Vevey',
-            color: '#fff',
             lat: 46.4628,
-            lon: 6.8419
+            lon: 6.8419,
+            name: 'Vevey'
           }, {
-            name: 'Cully',
-            color: '#fff',
             lat: 46.4896,
-            lon: 6.7291
+            lon: 6.7291,
+            name: 'Cully'
           }, {
-            name: 'Nyon',
-            color: '#fff',
             lat: 46.3833,
-            lon: 6.2348
+            lon: 6.2348,
+            name: 'Nyon'
           }, {
-            name: 'Lausanne',
-            color: '#fff',
             lat: 46.5197,
-            lon: 6.6323
+            lon: 6.6323,
+            name: 'Lausanne'
           }, {
-            name: 'Montreux',
-            color: '#fff',
             lat: 46.4312,
-            lon: 6.9107
+            lon: 6.9107,
+            name: 'Montreux'
           }, {
-            name: 'Lugano',
-            color: '#fff',
             lat: 46.0037,
-            lon: 8.9511
+            lon: 8.9511,
+            name: 'Lugano'
           }, {
-            name: 'Saint-Cergue',
-            color: '#fff',
             lat: 46.4460,
-            lon: 6.1581
+            lon: 6.1581,
+            name: 'Saint-Cergue'
           }, {
-            name: 'Hermance',
-            color: '#fff',
             lat: 46.3022,
-            lon: 6.2444
+            lon: 6.2444,
+            name: 'Hermance'
           }, {
-            name: 'Gruyères',
-            color: '#fff',
             lat: 46.5778,
-            lon: 7.0625
+            lon: 7.0625,
+            name: 'Gruyères'
           }, {
-            name: 'Le Pont',
-            color: '#fff',
             lat: 46.6496,
-            lon: 6.3203
+            lon: 6.3203,
+            name: 'Le Pont'
           }, {
-            name: 'Leysin',
-            color: '#fff',
             lat: 46.3436,
-            lon: 7.0120
+            lon: 7.0120,
+            name: 'Leysin'
           }, {
-            name: 'Verbier',
-            color: '#fff',
             lat: 46.0961,
-            lon: 7.2286
-          }]
+            lon: 7.2286,
+            name: 'Verbier'
+          }],
+          marker: {
+            symbol: 'circle',
+          },
+          name: 'Visited cities',
+          tooltip: {
+            headerFormat: '',
+            pointFormat: '<b>{point.name}</b>'
+          },
+          type: 'mappoint'
         }],
         title: {
           text: null
